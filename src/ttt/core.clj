8c4d090fac1d7c801cc9e0f11b398e16d842be8e
quephird <danielle_kefford@hotmail.com>
2016-10-10 13:14:51 -0400

    Added trivial mouse handler to add move to board.

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -11,7 +11,6 @@
   (quil/stroke-weight 10)
   (quil/ellipse-mode :center)
   (quil/no-fill)
-  (quil/no-loop)
   (let [new-session (rules/mk-session 'ttt.facts 'ttt.graphics 'ttt.rules)
         new-board   (facts/make-new-board)
         session-with-board (apply rules/insert new-session new-board)]
@@ -23,24 +22,28 @@
 (defn update [state]
   state)
 
+(defn mouse-clicked [state event]
+  (let [new-move (facts/make-new-move 0 2 :nought)
+        new-state (-> state
+                    (rules/insert new-move)
+                    (rules/fire-rules))]
+    new-state))
+
 ;; Draw current board
 (defn draw [state]
   ;; This is not a permanent strategy; it is just to test that new Move
   ;; facts can be added to the session, the rules can be fired, and the
   ;; board updated accordingly.
-  (let [new-move (facts/make-new-move 0 2 :nought)
-        new-state (-> state
-                    (rules/insert new-move)
-                    (rules/fire-rules))]
-    (graphics/board new-state)))
+  (graphics/board state))
 
 ;; Mouse handlers
 
 (quil/sketch
-  :title      "tic tac toe"
-  :size       [800 800]
-  :setup      setup
-  :update     update
-  :draw       draw
-  :middleware [m/fun-mode]
+  :title         "tic tac toe with quil and clara"
+  :size          [800 800]
+  :setup         setup
+  :update        update
+  :draw          draw
+  :mouse-clicked mouse-clicked
+  :middleware    [m/fun-mode]
   )
