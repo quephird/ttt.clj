79fc6c73d51bd34821b49bc28709345e3d505025
quephird <danielle_kefford@hotmail.com>
2016-10-11 00:13:42 -0400

    Moved query out of ttt.graphics back into ttt.queries.

--- src/ttt/queries.clj
+++ src/ttt/queries.clj
@@ -3,5 +3,8 @@
             [ttt.facts])
   (:import (ttt.facts Square)))
 
-(defquery find-square []
-  [?square <- Square])
+(defquery find-square [:?x :?y]
+  [?square <- Square (= ?x x) (= ?y y)])
+
+(defn select-square [state x y]
+  (first (query state find-square :?x x :?y y)))
